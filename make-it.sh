2910c0eb261af51992c667d70f20b0d94fe8164f
CCHall <explosivegnome@yahoo.com>
2015-02-27 19:42:22 -0500

    improved the build script

--- make-it.sh
+++ make-it.sh
@@ -2,9 +2,10 @@
 CURRENT_DIR="`dirname "$0"`"
 cd "${CURRENT_DIR}"
 
-rm -v build/libs/*.jar build/dev/*
-./gradlew jar
 mkdir -p build/dev
+mkdir -p build/dist
+rm -v build/libs/*.jar build/dev/* build/dist/*
+./gradlew jar
 cp -vf build/libs/*.jar build/dev/
 ./gradlew build javadoc
 cd build/docs/javadoc/
@@ -12,3 +13,13 @@ zip -r --quiet ../../dev/javadoc *
 cd "../../../src/main/java/"
 zip -r --quiet ../../../build/dev/src *
 cd "../../.."
+cd "build/dev"
+ORIG_NAME="`ls PowerAdvantage-*.jar`"
+API_INSERT="-dev-"
+zip -d "${ORIG_NAME}" assets/\*
+mv -v "${ORIG_NAME}" "${ORIG_NAME/-/$API_INSERT}"
+mv -v src.zip PowerAdvantage-src.zip
+mv -v javadoc.zip PowerAdvantage-javadoc.zip
+cd "../.."
+cp -vf build/libs/*.jar build/dev/* build/dist
+
