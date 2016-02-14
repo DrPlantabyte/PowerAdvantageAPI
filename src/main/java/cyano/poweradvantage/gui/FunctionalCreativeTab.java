92ff8cf7ab27f0159bf515c41778c5e9e2e0e292
Cyanobacterium <explosivegnome@yahoo.com>
2016-02-13 21:09:16 -0500

    creative tab upgrade

---
+++ src/main/java/cyano/poweradvantage/gui/FunctionalCreativeTab.java
@@ -0,0 +1,76 @@
+package cyano.poweradvantage.gui;
+
+import java.util.List;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+/**
+ * Uses java 8 function API to lazily pass an item icon
+ * @author DrCyano
+ *
+ */
+public class FunctionalCreativeTab  extends CreativeTabs {
+	
+	private final java.util.function.Supplier<Item> itemSupplier;
+	private final java.util.Comparator<ItemStack> itemSortingAlgorithm;
+	private final boolean searchEnabled;
+	/**
+	 * Constructor
+	 * @param unlocalizedName Name for translation
+	 * @param itemSupplier Function that provides the item used for the icon
+	 * @param itemSortingAlgorithm Algorithm for sorting the items (Comparable interface implementation)
+	 * @param searchBar If true, there will be a search bar above th item gallery
+	 */
+	public FunctionalCreativeTab( String unlocalizedName,
+			final java.util.function.Supplier<Item> itemSupplier,
+			final java.util.function.BiFunction<ItemStack,ItemStack,Integer> itemSortingAlgorithm,
+			boolean searchBar) {
+		super(unlocalizedName);
+		this.itemSupplier = itemSupplier;
+		this.itemSortingAlgorithm = new java.util.Comparator<ItemStack>(){
+			@Override
+			public int compare(ItemStack o1, ItemStack o2) {
+				return itemSortingAlgorithm.apply(o1, o2);
+			}
+		};
+		this.searchEnabled = searchBar;
+		if(this.searchEnabled)setBackgroundImageName("item_search.png");
+	}
+
+
+	/**
+	 * Determines if the search bar should be shown for this tab.
+	 *
+	 * @return True to show the bar
+	 */
+	@Override
+	public boolean hasSearchBar() {
+		return this.searchEnabled;
+	}
+	
+
+	/**
+	 * only shows items which have tabToDisplayOn == this
+	 * @param itemList 
+	 */
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void displayAllReleventItems(List<ItemStack> itemList)
+	{
+		super.displayAllReleventItems(itemList);
+		itemList.sort(itemSortingAlgorithm);
+	}
+
+	
+	/**
+	 * Gets the item used in the tab icon
+	 */
+	@SideOnly(Side.CLIENT)
+	@Override
+	public Item getTabIconItem() {
+		return itemSupplier.get();
+	}
+}
--- src/main/java/cyano/poweradvantage/init/ItemGroups.java
+++ src/main/java/cyano/poweradvantage/init/ItemGroups.java
@@ -1,8 +1,13 @@
 package cyano.poweradvantage.init;
 
-import net.minecraft.creativetab.CreativeTabs;
 import cyano.poweradvantage.PowerAdvantage;
-import cyano.poweradvantage.gui.PowerAdvantageCreativeTab;
+import cyano.poweradvantage.gui.FunctionalCreativeTab;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameData;
 
 public class ItemGroups {
 	
@@ -12,8 +17,34 @@ public class ItemGroups {
 	public static void init(){
 		if(initDone) return;
 		
-		tab_powerAdvantage = new PowerAdvantageCreativeTab( PowerAdvantage.MODID);
+		tab_powerAdvantage = new FunctionalCreativeTab( PowerAdvantage.MODID,
+				()->cyano.poweradvantage.init.Items.sprocket,
+				ItemGroups::itemSort,
+				false);
 		
 		initDone = true;
+	}
+	
+	private static int itemSort(ItemStack A, ItemStack B){
+		String catA = catagorize(A);
+		String catB = catagorize(B);
+		return catA.compareToIgnoreCase(catB);
+	}
+
+	private static String catagorize(ItemStack i) {
+		StringBuilder sb = new StringBuilder();
+		Item item = i.getItem();
+		sb.append(GameData.getItemRegistry().getNameForObject(item).getResourceDomain());
+		if(item instanceof ITileEntityProvider){
+			sb.append("A");
+		} else if(item instanceof ItemBlock){
+			sb.append("B");
+		} else {
+			sb.append("I");
+		}
+		sb.append(item.getUnlocalizedName());
+		sb.append(i.getMetadata());
+		
+		return sb.toString();
 	}
 }
