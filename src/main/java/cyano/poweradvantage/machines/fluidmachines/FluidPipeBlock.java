835b91e05289e02fdd8ce3b222315a89005eb9f0
Cyanobacterium <explosivegnome@yahoo.com>
2016-02-23 20:48:50 -0500

    working on better fluid compatibility

--- src/main/java/cyano/poweradvantage/machines/fluidmachines/FluidPipeBlock.java
+++ src/main/java/cyano/poweradvantage/machines/fluidmachines/FluidPipeBlock.java
@@ -6,6 +6,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
@@ -24,12 +26,25 @@ public class FluidPipeBlock extends BlockSimpleFluidConduit{
 	 * Called when a neighboring block changes.
 	 */
 	public void onNeighborBlockChange(World w, BlockPos pos, IBlockState state, Block neighborBlock){
+		pipeCheck(w,pos);
+	}
+	
+	/**
+	 * Called by ItemBlocks after a block is set in the world, to allow post-place logic
+	 */
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack){
+		super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+		pipeCheck(worldIn,pos);
+	}
+
+	private void pipeCheck(World w, BlockPos pos) {
 		if(!w.isRemote){
 			if(numberOfAdjacentFluidHandlers(w,pos) > 0){
 				w.setBlockState(pos, Blocks.fluid_pipe_terminal.getDefaultState());
 			}
 		}
 	}
+
 
 	protected static int numberOfAdjacentFluidHandlers(World w, BlockPos pos){
 		int sum = 0;
--- src/main/java/cyano/poweradvantage/machines/fluidmachines/modsupport/TerminalFluidPipeBlock.java
+++ src/main/java/cyano/poweradvantage/machines/fluidmachines/modsupport/TerminalFluidPipeBlock.java
@@ -5,6 +5,8 @@ import cyano.poweradvantage.machines.fluidmachines.FluidPipeBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
@@ -26,6 +28,18 @@ public class TerminalFluidPipeBlock extends FluidPipeBlock implements ITileEntit
 	 * Called when a neighboring block changes.
 	 */
 	public void onNeighborBlockChange(World w, BlockPos pos, IBlockState state, Block neighborBlock){
+		pipeCheck(w,pos);
+	}
+	
+	/**
+	 * Called by ItemBlocks after a block is set in the world, to allow post-place logic
+	 */
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack){
+		super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+		pipeCheck(worldIn,pos);
+	}
+
+	private void pipeCheck(World w, BlockPos pos) {
 		if(!w.isRemote){
 			if(numberOfAdjacentFluidHandlers(w,pos) == 0){
 				w.setBlockState(pos, Blocks.fluid_pipe.getDefaultState());
