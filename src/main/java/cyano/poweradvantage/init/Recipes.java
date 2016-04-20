e1dc8cc8acb9e8e54a266c78900e274546c13899
Chris Hall <explosivegnome@yahoo.com>
2016-04-20 06:51:48 -0400

    preparing for next version of Base Metals, which will have its own gear items

--- src/main/java/cyano/poweradvantage/init/Recipes.java
+++ src/main/java/cyano/poweradvantage/init/Recipes.java
@@ -6,7 +6,6 @@ import cyano.poweradvantage.PowerAdvantage;
 import cyano.poweradvantage.RecipeMode;
 import cyano.poweradvantage.registry.still.recipe.DistillationRecipeRegistry;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
@@ -62,7 +61,7 @@ public abstract class Recipes {
 		// recipe modes
 		if(PowerAdvantage.recipeMode == RecipeMode.NORMAL){
 			// normal means easy
-			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.sprocket,4)," x ","x x"," x ",'x',"ingotSteel"));
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.sprocket,4)," x ","x/x"," x ",'x',"ingotSteel",'/',"stickWood"));
 			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.sprocket,4)," x ","x x"," x ",'x',"ingotIron"));
 
 			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Blocks.fluid_pipe,6),"xxx","   ","xxx",'x',"ingotIron"));
@@ -71,7 +70,7 @@ public abstract class Recipes {
 		}else if(PowerAdvantage.recipeMode == RecipeMode.TECH_PROGRESSION){
 			// make things a little more complicated with tech-progression mode
 			BaseMetals.strongHammers = false;
-			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.sprocket,4)," x ","x/x"," x ",'x',"ingotSteel",'/',"stickWood"));
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.sprocket,4)," x ","x/x"," x ",'x',"ingotSteel",'/',"rod"));
 			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Blocks.fluid_pipe,6),"xxx","   ","xxx",'x',"ingotIron"));
 		}else if(PowerAdvantage.recipeMode == RecipeMode.APOCALYPTIC){
 			// apocalyptic means some things are not craftable, but some stuff can be recycled
